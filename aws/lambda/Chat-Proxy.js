2b6496897452dbc27faa2b85593b26b50f7f73d4
Kirk Rohani <kirk.rohani@gmail.com>
2023-07-21 18:51:16 -0500

    updated lambda proxy, updated lambda code and updated conversations.json file to add a 3rd convo

--- aws/lambda/Chat-Proxy.js
+++ aws/lambda/Chat-Proxy.js
@@ -3,8 +3,9 @@
 var AWS = require('aws-sdk');
 
 var S3 = new AWS.S3();
+var dynamo = new AWS.DynamoDB();
 
-var bucket = 'aicloudchat.com';
+var bucket = '<your bucket name>';
 
 exports.handler = function (event, context, callback) {
 
@@ -14,7 +15,7 @@ exports.handler = function (event, context, callback) {
             body: err ? JSON.stringify(err) : JSON.stringify(res),
             headers: {
                 'Content-Type': 'application/json',
-                'Access-Control-Allow-Origin': 'http://aicloudchat.com.s3-website-us-east-1.amazonaws.com'
+                'Access-Control-Allow-Origin': '*'
             }
         });
     };
@@ -30,13 +31,69 @@ exports.handler = function (event, context, callback) {
         });
     } else if (path.startsWith('conversations/')) {
         var id = path.substring('conversations/'.length);
-        S3.getObject({
-            Bucket: bucket,
-            Key: 'data/conversations/' + id + '.json'
+        dynamo.query({
+            TableName: 'Chat-Messages',
+            ProjectionExpression: '#T, Sender, Message',
+            ExpressionAttributeNames: {'#T': 'Timestamp'},
+            KeyConditionExpression: 'ConversationId = :id',
+            ExpressionAttributeValues: {':id': {S: id}}
         }, function (err, data) {
-            done(err, err ? null : JSON.parse(data.Body.toString()));
+            loadMessages(err, data, id, [], done);
         });
     } else {
         done('No cases hit');
     }
 };
+
+function loadMessages(err, data, id, messages, callback) {
+    if (err === null) {
+        data.Items.forEach(function (message) {
+            messages.push({
+                sender: message.Sender.S,
+                time: Number(message.Timestamp.N),
+                message: message.Message.S
+            });
+        });
+        if(data.LastEvaluatedKey) {
+            dynamo.query({
+                TableName: 'Chat-Messages',
+                ProjectionExpression: '#T, Sender, Message',
+                KeyConditionExpression: 'ConversationId = :id',
+                ExpressionAttributeNames: {'#T': 'Timestamp'},
+                ExpressionAttributeValues: {':id': {S: id}},
+                ExclusiveStartKey: data.LastEvaluatedKey
+            }, function (err, data) {
+                loadMessages(err, data, id, messages, callback);
+            });
+        } else {
+            loadConversationDetail(id, messages, callback);
+        }
+    } else {
+        callback(err);
+    }
+}
+
+function loadConversationDetail(id, messages, callback) {
+    dynamo.query({
+        TableName: 'Chat-Conversations',
+        Select: 'ALL_ATTRIBUTES',
+        KeyConditionExpression: 'ConversationId = :id',
+        ExpressionAttributeValues: {':id': {S: id}}
+    }, function (err, data) {
+        if (err === null) {
+            var participants = [];
+            data.Items.forEach(function (item) {
+                participants.push(item.Username.S);
+            });
+
+            callback(null, {
+                id: id,
+                participants: participants,
+                last: messages.length > 0 ? messages[messages.length-1].time : undefined,
+                messages: messages
+            });
+        } else {
+            callback(err);
+        }
+    });
+}
